511b6d672ea24ad7db26d664d1065ef4bbcf08f6
Ward Huddleston <whuddles@gmail.com>
2019-08-05 15:41:11 -0400

    working on models and dals for sprint 1

---
+++ WebApplication.Web/DAL/RecipeSqlDAL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Web.Models;
+
+namespace WebApplication.Web.DAL
+{
+    public class RecipeSqlDAL : IRecipeDAL
+    {
+        private string connectionString = "";
+
+        public RecipeSqlDAL(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public List<Recipe> GetRecipes()
+        {
+            List<Recipe> Recipes = new List<Recipe>();
+
+            return Recipes;
+
+        }
+
+    }
+}
---
+++ WebApplication.Web/Models/Recipe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Web.Models
+{
+    public class Recipe
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Instructions { get; set; }
+        public int PrepTime { get; set; }
+        public int CookTime { get; set; }
+        public int TotalTime { get; set; }
+
+        public List<Ingredient> Ingredients { get; set; }
+
+    }
+}
