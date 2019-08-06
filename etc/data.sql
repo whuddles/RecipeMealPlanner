d5382a16c7aa3e967579a09471efbd1bb91e1534
Ward Huddleston <whuddles@gmail.com>
2019-08-06 10:14:54 -0400

    cleanup

--- etc/data.sql
+++ etc/data.sql
@@ -44,40 +44,109 @@ CREATE TABLE recipe
 	cook_time		int				not null DEFAULT(0)
 );
 
-CREATE TABLE recipe_ingredient
+CREATE TABLE number
+(
+	number_id		int			IDENTITY			PRIMARY KEY,
+	number			varchar(3)	--NOT NULL			DEFAULT(0)
+);
+
+CREATE TABLE fraction
+(
+	fraction_id		int			IDENTITY			PRIMARY KEY,
+	fraction		varchar(3)  --NOT NULL			DEFAULT(0)
+);
+
+CREATE TABLE unit
+(
+	unit_id			INT			IDENTITY			PRIMARY KEY,
+	unit			VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE recipe_ingredient_unit_number_fraction
 (
 	recipe_id		int		FOREIGN KEY REFERENCES recipe(recipe_id),
-	ingredient_id	int		FOREIGN KEY REFERENCES ingredient(ingredient_id)
+	ingredient_id	int		FOREIGN KEY REFERENCES ingredient(ingredient_id),
+	unit_id			int		FOREIGN KEY REFERENCES unit(unit_id),
+	number_id		int		FOREIGN KEY REFERENCES number(number_id),
+	fraction_id		int		FOREIGN KEY REFERENCES fraction(fraction_id)
 
-	CONSTRAINT pk_recipe_ingredient PRIMARY KEY CLUSTERED (recipe_id, ingredient_id)
+	CONSTRAINT pk_recipe_ingredient PRIMARY KEY CLUSTERED (recipe_id, ingredient_id, unit_id, number_id, fraction_id)
 );
 
-CREATE TABLE quantity
-(
-	quantity_id		int			IDENTITY			PRIMARY KEY,
-	number			varchar(3)	NOT NULL DEFAULT(0),
-	fraction		varchar(3)	NOT NULL DEFAULT(0),
-	unit			varchar(20) NOT NULL			
-);
+--CREATE TABLE quantity_ingredient
+--(
+--	quantity_id			int			FOREIGN KEY REFERENCES quantity(quantity_id),
+--	ingredient_id		int			FOREIGN KEY REFERENCES ingredient(ingredient_id)
 
-CREATE TABLE quantity_ingredient
-(
-	quantity_id			int			FOREIGN KEY REFERENCES quantity(quantity_id),
-	ingredient_id	int			FOREIGN KEY REFERENCES ingredient(ingredient_id)
+--	CONSTRAINT pk_unit_ingredient PRIMARY KEY CLUSTERED (quantity_id, ingredient_id)
+--);
 
-	CONSTRAINT pk_unit_ingredient PRIMARY KEY CLUSTERED (quantity_id, ingredient_id)
-);
+SET IDENTITY_INSERT unit ON;
 
-SET IDENTITY_INSERT quantity ON;
+INSERT INTO unit (unit_id, unit) VALUES (1, 'item(s)');
+INSERT INTO unit (unit_id, unit) VALUES (2, 'slice(s)');
+INSERT INTO unit (unit_id, unit) VALUES (3, 'piece(s)');
+INSERT INTO unit (unit_id, unit) VALUES (4, 'fl. oz.');
+INSERT INTO unit (unit_id, unit) VALUES (5, 'cup');
+INSERT INTO unit (unit_id, unit) VALUES (6, 'pint');
+INSERT INTO unit (unit_id, unit) VALUES (7, 'quart');
+INSERT INTO unit (unit_id, unit) VALUES (8, 'gallon');
+INSERT INTO unit (unit_id, unit) VALUES (9, 'tsp.');
+INSERT INTO unit (unit_id, unit) VALUES (10, 'Tbsp.');
+INSERT INTO unit (unit_id, unit) VALUES (11, 'oz.');
+INSERT INTO unit (unit_id, unit) VALUES (12, 'lb.');
+INSERT INTO unit (unit_id, unit) VALUES (13, 'pinch');
+INSERT INTO unit (unit_id, unit) VALUES (14, 'dash');
+INSERT INTO unit (unit_id, unit) VALUES (15, 'jigger');
 
-INSERT INTO quantity (quantity_id, number, fraction, unit) VALUES (1, '2', '', 'slices');
-INSERT INTO quantity (quantity_id, number, fraction, unit) VALUES (2, '2', '', 'Tbsp.');
-INSERT INTO quantity (quantity_id, number, fraction, unit) VALUES (3, '1', '', 'Tbsp.');
-INSERT INTO quantity (quantity_id, number, fraction, unit) VALUES (4, '3', '', '');
-INSERT INTO quantity (quantity_id, number, fraction, unit) VALUES (5, '1', '', '');
-INSERT INTO quantity (quantity_id, number, fraction, unit) VALUES (6, '12', '', 'ounce');
+SET IDENTITY_INSERT unit OFF;
 
-SET IDENTITY_INSERT quantity OFF;
+SET IDENTITY_INSERT number ON;
+
+INSERT INTO number (number_id, number) VALUES (0, '0');
+INSERT INTO number (number_id, number) VALUES (1, '1');
+INSERT INTO number (number_id, number) VALUES (2, '2');
+INSERT INTO number (number_id, number) VALUES (3, '3');
+INSERT INTO number (number_id, number) VALUES (4, '4');
+INSERT INTO number (number_id, number) VALUES (5, '5');
+INSERT INTO number (number_id, number) VALUES (6, '6');
+INSERT INTO number (number_id, number) VALUES (7, '7');
+INSERT INTO number (number_id, number) VALUES (8, '8');
+INSERT INTO number (number_id, number) VALUES (9, '9');
+INSERT INTO number (number_id, number) VALUES (10, '10');
+INSERT INTO number (number_id, number) VALUES (11, '11');
+INSERT INTO number (number_id, number) VALUES (12, '12');
+INSERT INTO number (number_id, number) VALUES (13, '13');
+INSERT INTO number (number_id, number) VALUES (14, '14');
+INSERT INTO number (number_id, number) VALUES (15, '15');
+INSERT INTO number (number_id, number) VALUES (16, '16');
+INSERT INTO number (number_id, number) VALUES (17, '17');
+INSERT INTO number (number_id, number) VALUES (18, '18');
+INSERT INTO number (number_id, number) VALUES (19, '19');
+INSERT INTO number (number_id, number) VALUES (20, '20');
+----INSERT INTO number (number_id, number) VALUES (1, '2', '', 'slices');
+----INSERT INTO number (number_id, number) VALUES (2, '2', '', 'Tbsp.');
+----INSERT INTO number (number_id, number) VALUES (3, '1', '', 'Tbsp.');
+----INSERT INTO number (number_id, number) VALUES (4, '3', '', '');
+----INSERT INTO number (number_id, number) VALUES (5, '1', '', '');
+----INSERT INTO number (number_id, number) VALUES (6, '12', '', 'ounce');
+
+SET IDENTITY_INSERT number OFF;
+
+SET IDENTITY_INSERT fraction ON
+
+INSERT INTO fraction (fraction_id, fraction) VALUES (0, '');
+INSERT INTO fraction (fraction_id, fraction) VALUES (1, '1/8');
+INSERT INTO fraction (fraction_id, fraction) VALUES (2, '1/4');
+INSERT INTO fraction (fraction_id, fraction) VALUES (3, '1/3');
+INSERT INTO fraction (fraction_id, fraction) VALUES (4, '3/8');
+INSERT INTO fraction (fraction_id, fraction) VALUES (5, '1/2');
+INSERT INTO fraction (fraction_id, fraction) VALUES (6, '5/8');
+INSERT INTO fraction (fraction_id, fraction) VALUES (7, '2/3');
+INSERT INTO fraction (fraction_id, fraction) VALUES (8, '3/4');
+INSERT INTO fraction (fraction_id, fraction) VALUES (9, '7/8');
+
+SET IDENTITY_INSERT fraction OFF
 
 SET IDENTITY_INSERT ingredient ON;
 
@@ -99,22 +168,22 @@ INSERT INTO recipe (recipe_id, name, description, instructions, prep_time, cook_
 
 SET IDENTITY_INSERT recipe OFF;
 
-INSERT INTO recipe_ingredient (recipe_id, ingredient_id) VALUES (1, 1);
-INSERT INTO recipe_ingredient (recipe_id, ingredient_id) VALUES (1, 2);
-INSERT INTO recipe_ingredient (recipe_id, ingredient_id) VALUES (1, 3);
-INSERT INTO recipe_ingredient (recipe_id, ingredient_id) VALUES (2, 4);
-INSERT INTO recipe_ingredient (recipe_id, ingredient_id) VALUES (2, 5);
-INSERT INTO recipe_ingredient (recipe_id, ingredient_id) VALUES (2, 6);
-INSERT INTO recipe_ingredient (recipe_id, ingredient_id) VALUES (2, 7);
-INSERT INTO recipe_ingredient (recipe_id, ingredient_id) VALUES (2, 8);
+INSERT INTO recipe_ingredient_unit_number_fraction (recipe_id, ingredient_id, unit_id, number_id, fraction_id) VALUES (1, 1, 2, 2, 0);
+INSERT INTO recipe_ingredient_unit_number_fraction (recipe_id, ingredient_id, unit_id, number_id, fraction_id) VALUES (1, 2, 10, 2, 0);
+INSERT INTO recipe_ingredient_unit_number_fraction (recipe_id, ingredient_id, unit_id, number_id, fraction_id) VALUES (1, 3, 10, 2, 0);
+INSERT INTO recipe_ingredient_unit_number_fraction (recipe_id, ingredient_id, unit_id, number_id, fraction_id) VALUES (2, 4, 1, 3, 0);
+INSERT INTO recipe_ingredient_unit_number_fraction (recipe_id, ingredient_id, unit_id, number_id, fraction_id) VALUES (2, 5, 10, 1, 5);
+INSERT INTO recipe_ingredient_unit_number_fraction (recipe_id, ingredient_id, unit_id, number_id, fraction_id) VALUES (2, 6, 1, 0, 3);
+INSERT INTO recipe_ingredient_unit_number_fraction (recipe_id, ingredient_id, unit_id, number_id, fraction_id) VALUES (2, 7, 11, 2, 0);
+INSERT INTO recipe_ingredient_unit_number_fraction (recipe_id, ingredient_id, unit_id, number_id, fraction_id) VALUES (2, 8, 10, 2, 0);
 
-INSERT INTO quantity_ingredient (quantity_id, ingredient_id) VALUES (1, 1);
-INSERT INTO quantity_ingredient (quantity_id, ingredient_id) VALUES (2, 2);
-INSERT INTO quantity_ingredient (quantity_id, ingredient_id) VALUES (2, 3);
-INSERT INTO quantity_ingredient (quantity_id, ingredient_id) VALUES (4, 4);
-INSERT INTO quantity_ingredient (quantity_id, ingredient_id) VALUES (2, 5);
-INSERT INTO quantity_ingredient (quantity_id, ingredient_id) VALUES (5, 6);
-INSERT INTO quantity_ingredient (quantity_id, ingredient_id) VALUES (6, 7);
-INSERT INTO quantity_ingredient (quantity_id, ingredient_id) VALUES (3, 8);
+--INSERT INTO quantity_ingredient (quantity_id, ingredient_id) VALUES (1, 1);
+--INSERT INTO quantity_ingredient (quantity_id, ingredient_id) VALUES (2, 2);
+--INSERT INTO quantity_ingredient (quantity_id, ingredient_id) VALUES (2, 3);
+--INSERT INTO quantity_ingredient (quantity_id, ingredient_id) VALUES (4, 4);
+--INSERT INTO quantity_ingredient (quantity_id, ingredient_id) VALUES (2, 5);
+--INSERT INTO quantity_ingredient (quantity_id, ingredient_id) VALUES (5, 6);
+--INSERT INTO quantity_ingredient (quantity_id, ingredient_id) VALUES (6, 7);
+--INSERT INTO quantity_ingredient (quantity_id, ingredient_id) VALUES (3, 8);
 
 COMMIT TRANSACTION;
