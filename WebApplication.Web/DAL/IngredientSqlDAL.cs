00b382416d8224bdcb4b5b0ca520dc6396d708ab
Jesse Blanton <jblanton.eng@gmail.com>
2019-08-06 14:06:26 -0400

    add ingredientId mapping to MapRowToIngredient

--- WebApplication.Web/DAL/IngredientSqlDAL.cs
+++ WebApplication.Web/DAL/IngredientSqlDAL.cs
@@ -10,17 +10,13 @@ namespace WebApplication.Web.DAL
     public class IngredientSqlDAL : IIngredientDAL
     {
         private string connectionString = ""; //todo what is the connection string to our db?
-        private string sqlQueryGetIngredients = ""; //todo create SQL query to get name, quantity, unit type from db
-        private string sqlInsertIngredient = "INSERT INTO ingredient VALUES(@name)"; //todo create sql insert to add ingredient with quantity and unit to db
+        private string sqlQueryGetIngredients = "SELECT * FROM ingredient"; //todo create SQL query to get name, quantity, unit type from db
+        private string sqlInsertIngredient = "INSERT INTO ingredient VALUES(@name)";
         private string SqlFilterNewIngredientQuery = "SELECT name FROM ingredient WHERE name = @name";
 
         public IngredientSqlDAL(string connectionString)
         {
             this.connectionString = connectionString;
-        }
-
-        public IngredientSqlDAL()
-        {
         }
 
         public List<Ingredient> GetIngredients()
@@ -51,6 +47,8 @@ namespace WebApplication.Web.DAL
         private Ingredient MapRowToIngredient(SqlDataReader reader)
         {
             Ingredient ingredient = new Ingredient();
+
+            ingredient.IngredientId = Convert.ToInt32(reader["ingredient_id"]);
             ingredient.Name = Convert.ToString(reader["name"]);
 
             return ingredient;
@@ -127,6 +125,5 @@ namespace WebApplication.Web.DAL
             return newIngredients;
 
         }
-
     }
 }
