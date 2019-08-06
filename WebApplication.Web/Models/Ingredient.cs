f67742d1f5617464b043f3ec5e4393aaa01518fa
Jesse Blanton <jblanton.eng@gmail.com>
2019-08-06 12:48:37 -0400

    add id properties

--- WebApplication.Web/Models/Ingredient.cs
+++ WebApplication.Web/Models/Ingredient.cs
@@ -7,9 +7,12 @@ namespace WebApplication.Web.Models
 {
     public class Ingredient
     {
+        public int IngredientId { get; set; }
         public string Name { get; set; }
+        public int UnitId { get; set; }
         public string Unit { get; set; }
         public string Number { get; set; }
+        public int FractionId { get; set; }
         public string Fraction { get; set; }
     }
 }
--- WebApplication.Web/Models/Recipe.cs
+++ WebApplication.Web/Models/Recipe.cs
@@ -7,14 +7,13 @@ namespace WebApplication.Web.Models
 {
     public class Recipe
     {
+        public int RecipeId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Instructions { get; set; }
         public int PrepTime { get; set; }
         public int CookTime { get; set; }
         public int TotalTime { get; set; }
-
         public List<Ingredient> Ingredients { get; set; }
-
     }
 }
