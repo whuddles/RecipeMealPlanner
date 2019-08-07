bf382e31400dbf3e2219c45c25e5cacae2f2a18a
Jesse Blanton <jblanton.eng@gmail.com>
2019-08-07 14:34:07 -0400

    create AddIngredient functionality, make ingredient list searchable

--- WebApplication.Web/Controllers/RecipeController.cs
+++ WebApplication.Web/Controllers/RecipeController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication.Web.DAL;
 using WebApplication.Web.Models;
@@ -25,15 +27,60 @@ namespace WebApplication.Web.Controllers
             return View(recipe);
         }
 
+        [HttpGet]
         public IActionResult Create()
-        {
-            Recipe newRecipe = new Recipe();
-            ViewBag.ingredients = ingredientDAL.GetIngredients();
-            ViewBag.units = ingredientDAL.GetUnits();
-            ViewBag.numbers = ingredientDAL.GetNumbers();
-            ViewBag.fractions = ingredientDAL.GetFractions();
+        {            
+            string recipeStatus = HttpContext.Session.GetString("NewRecipeStatus");
+            ViewBag.RecipeStatus = recipeStatus;
+
+            ViewBag.ExistingIngredients = ingredientDAL.GetIngredients();
+            ViewBag.Units = ingredientDAL.GetUnits();
+            ViewBag.Numbers = ingredientDAL.GetNumbers();
+            ViewBag.Fractions = ingredientDAL.GetFractions();
             
             return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Create(Recipe recipe, List<Ingredient> ingredients)
+        {
+            if(!ModelState.IsValid)
+            {
+                HttpContext.Session.SetString("NewRecipeStatus", "Partial");
+
+                return RedirectToAction("Create");
+            }
+
+            recipe.Ingredients = ingredients;
+
+            bool recipeAdded = recipeDAL.AddRecipe(recipe);
+
+            if(recipeAdded)
+            {
+                HttpContext.Session.SetString("NewRecipeStatus", "Complete");
+
+                return RedirectToAction("Detail");
+            }
+
+            return RedirectToAction("Create");
+        }
+
+        [HttpGet]
+        public IActionResult AddIngredient()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddIngredient(string newIngredient)
+        {
+            string[] newIngredients = newIngredient.Split(", ");
+            foreach(string str in newIngredients)
+            {
+                ingredientDAL.AddIngredient(str);
+            }
+
+            return RedirectToAction("Create");
         }
     }
 }
